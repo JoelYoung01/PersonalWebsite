7489e321697c1a1e1b71accd4aa0d295edc302a9
Joel Young [remote] <joeleyoung01@gmail.com>
2022-03-28 20:53:36 -0500

    removed annoying title template

--- nuxt.config.js
+++ nuxt.config.js
@@ -6,7 +6,7 @@ export default {
 
    // Global page headers: https://go.nuxtjs.dev/config-head
    head: {
-      titleTemplate: '%s - node_server',
+      titleTemplate: '',
       title: 'Joel\'s Node Server',
       htmlAttrs: {
          lang: 'en'
