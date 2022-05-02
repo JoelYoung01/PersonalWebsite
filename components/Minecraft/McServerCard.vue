cbaa03459dac9c4daf111f2a36a29f20b76da192
YoungJoel <joeleyoung01@gmail.com>
2022-05-02 09:17:18 -0500

    added MC server component

---
+++ components/Minecraft/McServerCard.vue
@@ -0,0 +1,19 @@
+<template>
+  <v-card>
+    <v-card-title>{{ title ? title : "Mc Server" }}</v-card-title>
+  </v-card>
+</template>
+<script lang="ts">
+import { Component, Vue, Prop } from "nuxt-property-decorator";
+
+@Component({
+  name: "McServerCard",
+})
+export default class McServerCard extends Vue {
+  @Prop(String)
+  readonly address!: string;
+
+  @Prop(String)
+  readonly title!: string;
+}
+</script>
