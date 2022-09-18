bb9caa010117aa4b2a63155a907275c9b611cf26
Joel Young [Jooser PC] <joeleyoung01@gmail.com>
2022-09-18 18:14:04 -0500

    hardcoded google analytics tag

--- plugins/vue-gtag.js
+++ plugins/vue-gtag.js
@@ -1,7 +1,6 @@
 import Vue from 'vue'
 import VueGtag from 'vue-gtag'
 
-console.log(`using gtag ${process.env.GOOGLE_ANALYTICS_ID}`)
 Vue.use(VueGtag, {
-  config: { id: process.env.GOOGLE_ANALYTICS_ID }
+  config: { id: 'G-SPGXJXNGDR' }
 })
