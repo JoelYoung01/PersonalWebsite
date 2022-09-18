b2f6ad166026c8db50e8a50ed8606f6560029f45
[DockerServer] Joel <joeleyoung01@gmail.com>
2022-09-18 23:02:25 +0000

    testing

--- plugins/vue-gtag.js
+++ plugins/vue-gtag.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import VueGtag from 'vue-gtag'
 
+console.log(`using gtag ${process.env.GOOGLE_ANALYTICS_ID}`)
 Vue.use(VueGtag, {
   config: { id: process.env.GOOGLE_ANALYTICS_ID }
 })
