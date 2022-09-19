9243eed93889af423a7849ee630b73fa24bcefc5
Joel Young [Jooser PC] <joeleyoung01@gmail.com>
2022-09-18 21:06:08 -0500

    testing ga

--- pages/projects.vue
+++ pages/projects.vue
@@ -5,11 +5,21 @@
         <div class="text-h2">Personal Projects</div>
       </v-col>
     </v-row>
+    <v-row>
+      <v-col>
+        <v-btn @click="checkGoogleAnalytics">Test</v-btn>
+      </v-col>
+    </v-row>
   </v-container>
 </template>
 
 <script>
 export default {
   name: "projects",
+  methods: {
+    checkGoogleAnalytics() {
+      this.$gtag.event("testClicked", { method: "Google" });
+    },
+  },
 };
 </script>
