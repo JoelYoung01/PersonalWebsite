b52a1aa7d61dbc25a49644f812fb79131556e19d
Joel Young [Jooser PC] <joeleyoung01@gmail.com>
2023-02-18 12:22:34 -0600

    Ditch Nuxt for a pure Vue3 project

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,15 +1,29 @@
+/* eslint-env node */
+require("@rushstack/eslint-patch/modern-module-resolution");
+
 module.exports = {
   root: true,
   env: {
     browser: true,
     node: true
   },
-  plugins: ["vue", "@typescript-eslint"],
+  parserOptions: {
+    ecmaVersion: "latest"
+  },
   extends: [
-    "eslint:recommended",
-    "plugin:@typescript-eslint/recommended",
     "plugin:vue/vue3-recommended",
-    "@nuxtjs/eslint-config-typescript"
+    "eslint:recommended",
+    "@vue/eslint-config-typescript"
+  ],
+  overrides: [
+    {
+      files: [
+        "cypress/e2e/**/*.{cy,spec}.{js,ts,jsx,tsx}"
+      ],
+      "extends": [
+        "plugin:cypress/recommended"
+      ]
+    }
   ],
   rules: {
     quotes: ["warn", "double"],
---
+++ src/app.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <v-app>
+    <RouterView />
+  </v-app>
+</template>
