576f561aea3218a3696d3570c97c926f6ac1f82a
Joel Young [Jooser PC] <joeleyoung01@gmail.com>
2023-02-20 20:30:54 -0600

    Fixed 'notreadyyet' component to resize correctly

--- src/components/misc/NotReadyYet.vue
+++ src/components/misc/NotReadyYet.vue
@@ -1,16 +1,19 @@
 <script setup lang="ts">
 import RandomMeme from "@/components/RandomMeme.vue";
+import { useDisplay } from "vuetify/lib/framework.mjs";
+
+const { mobile } = useDisplay();
 </script>
 
 <template>
-  <v-card class="mx-auto text-center" width="400">
-    <v-card-title>
-      <v-icon icon="mdi-progress-wrench" class="mr-1" /> This page is not quite done yet!
-    </v-card-title>
+  <v-card class="mx-auto text-center" :width="mobile ? 325 : 375" prepend-icon="mdi-progress-wrench">
+    <template #title>
+      This page is not done yet!
+    </template>
     <v-card-subtitle class="mb-5">
       Coming soon to a website near you...<br>
       Here's a random meme from reddit instead!
     </v-card-subtitle>
-    <RandomMeme class="mx-auto mb-5" />
+    <RandomMeme class="mb-5" />
   </v-card>
 </template>
