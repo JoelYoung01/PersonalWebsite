c90a87d6e007c075921ecb1b43cdf4b501cff6f7
Joel Young [Jooser PC] <joeleyoung01@gmail.com>
2023-04-06 20:47:11 -0500

    Fixed unit tests

--- src/components/__tests__/unit/misc/NotReadyYet.test.ts
+++ src/components/__tests__/unit/misc/NotReadyYet.test.ts
@@ -4,12 +4,26 @@ import { useDisplay, type DisplayInstance } from "vuetify/lib/framework.mjs";
 import NotReadyYet from "@/components/misc/NotReadyYet.vue";
 import { ref } from "vue";
 
+class ResizeObserver {
+  observe() {
+    /**/
+  }
+  unobserve() {
+    /**/
+  }
+}
+
 vi.mock("vuetify/lib/framework.mjs", async (actual) => ({
   ...((await actual()) as object),
   useDisplay: vi.fn(() => ({ mobile: ref(false) }))
 }));
 
 describe("NotReadyYet.vue", () => {
+  beforeAll(() => {
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    window.ResizeObserver = ResizeObserver as any;
+  });
+
   test("renders the correct message", () => {
     const wrapper = mount(NotReadyYet, {
       global: {
