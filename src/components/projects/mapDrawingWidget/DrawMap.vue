152f9cd831e18be61b7f4a11429080036c6e478d
Joel Young [Jooser PC] <joeleyoung01@gmail.com>
2023-10-23 19:40:45 -0500

    Wired in the map

---
+++ src/components/projects/mapDrawingWidget/DrawMap.vue
@@ -0,0 +1,3 @@
+<template>
+  <GMapMap :center="{ lat: 51.093048, lng: 6.84212 }" :zoom="7" map-type-id="satellite" />
+</template>
--- src/views/Projects/MapDrawWidget.vue
+++ src/views/Projects/MapDrawWidget.vue
@@ -1,3 +1,10 @@
+<script setup lang="ts">
+import DrawMap from "@/components/projects/mapDrawingWidget/DrawMap.vue";
+</script>
+
 <template>
-  <div>Google Maps Widget goes here</div>
+  <div>
+    Google Maps Widget goes here
+    <DrawMap style="height: 500px; width: 700px" />
+  </div>
 </template>
