e68d8768e397fca1a70cb198121714ce5eac6dbf
Joel Young [Jooser PC] <joeleyoung01@gmail.com>
2023-11-05 18:46:46 -0600

    Finished wiring up google map widget

--- src/components/projects/mapDrawingWidget/DrawMap.vue
+++ src/components/projects/mapDrawingWidget/DrawMap.vue
@@ -2,25 +2,33 @@
 /// <reference types="google.maps" />
 import { ref } from "vue";
 import { GoogleMap } from "vue3-google-map";
+import DrawingManager from "./DrawingManager.vue";
 
 const sodSmith: google.maps.LatLngLiteral = { lat: 44.886297901877114, lng: -93.30808521796632 };
 
-const zoom = ref(5);
+const zoom = ref(17);
+const mapRef = ref<InstanceType<typeof GoogleMap>>();
 
 const apiKey = import.meta.env.VITE_GOOGLE_API_KEY;
 </script>
 
 <template>
   <GoogleMap
+    ref="mapRef"
     :background-color="$vuetify.theme.current.colors.background"
     :api-key="apiKey"
     :center="sodSmith"
     :zoom="zoom"
-    :max-zoom="15"
-    :min-zoom="1"
+    :max-zoom="20"
+    :min-zoom="16"
+    :tilt="0"
+    :libraries="[]"
+    disable-double-click-zoom
     map-type-id="satellite"
     disable-default-ui
     zoom-control
+    fullscreen-control
   >
+    <DrawingManager :map-ref="mapRef" />
   </GoogleMap>
 </template>
--- src/views/Projects/MapDrawWidget.vue
+++ src/views/Projects/MapDrawWidget.vue
@@ -1,10 +1,33 @@
 <script setup lang="ts">
 import DrawMap from "@/components/projects/mapDrawingWidget/DrawMap.vue";
+
+import { Loader } from "@googlemaps/js-api-loader";
+import { ref } from "vue";
+
+const loadingApi = ref(false);
+
+const initGoogleApi = async () => {
+  loadingApi.value = true;
+  try {
+    const loader = new Loader({
+      apiKey: import.meta.env.VITE_GOOGLE_API_KEY,
+      version: "weekly"
+    });
+    loader.importLibrary("drawing");
+    loader.importLibrary("places");
+  } catch (er) {
+    console.error(er);
+  }
+  loadingApi.value = false;
+};
+
+initGoogleApi();
 </script>
 
 <template>
   <div>
-    Google Maps Widget goes here
+    Google Maps Widget Demo {{ loadingApi ? " - Loading..." : "" }}
+
     <DrawMap style="height: 500px; width: 700px" />
   </div>
 </template>
