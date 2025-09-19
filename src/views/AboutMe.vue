cc620610e9eb7c23c30399dfe23c0b4c5b6dbb76
Joel Young [Jooser PC] <joeleyoung01@gmail.com>
2025-09-18 21:49:20 -0500

    Updated the about-me page

--- src/views/AboutMe.vue
+++ src/views/AboutMe.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import SocialCard from "@/components/aboutMe/SocialCard.vue";
+import svelteLogo from "@/assets/images/Svelte_Logo_bw.svg";
 import { useDisplay } from "vuetify";
 
 // Mobile flag (true if viewer is mobile user)
@@ -8,7 +9,8 @@ const { mobile } = useDisplay();
 interface Skill {
   title: string;
   description: string;
-  mdiIcon: string;
+  mdiIcon?: string;
+  svgIcon?: string;
 }
 
 interface Hobby {
@@ -28,29 +30,35 @@ const hardSkillList: Skill[] = [
   {
     title: "Vue.js",
     description:
-      "As I've stepped into the front-end framework scene, I've dipped my toe into the big three; Angular, React, and Vue. Although all three certainly have their place, I was drawn to Vue because of it's pretty manageable learning curve and vast community. The more I develop Vue.js apps, the more I really start to love the idea of a community-first front end framework. I recognize there is risks when putting eggs into the Vue basket, but I think that this project has really taken off in the recent years and really see the beauty of open source shine through it.",
+      "I've written countless Vue.js applications, both professionally and personally. I enjoy the simplicity and ease of use of Vue.js, the community support, and the wide range of toolings and libraries available. I've written Vue 2 and Vue 3 applications, using both Composition and Options API, and have migrated >20 applications from 2 to 3. And I still love the framework!",
     mdiIcon: "mdi-vuejs",
+  },
+  {
+    title: "Svelte",
+    description:
+      "I've written an app with Svelte and another with SvelteKit. Svelte is great for quick, small projects but can scale to larger projects with the right direction, which makes it a powerful tool for any developer.",
+    svgIcon: svelteLogo,
   },
   {
     title: "Typescript",
     description:
-      "I've been using Typescript for 3 years now, first tentavely as it seems just an overcomplication on JavaSript and now with much love. I have been saved by it through many projects; from React apps to Vue apps to just Node.js Scripts, and have never looked back to learning the ins and outs!",
+      "Typescript is a great tool that I've used for many large Vue projects professionally and personally. The peace of mind and robustness it provides is invaluable to larger scale projects that involve bigger teams.",
     mdiIcon: "mdi-language-typescript",
   },
   {
     title: ".NET",
     description:
-      "I have been writing in the .NET framework for 2 years in both front-end situations using MVC with Razor syntax and back-end situations writing resiliant SOAP and RESTful APIs. The nifty tricks that C# boasts along with the usability make it my favorite statically typed language. Before you write me off, Rust and GO are on my list to learn!",
+      ".NET has been a staple in my software development career for business critical applications that require reliability and maintainability. I've written both front-end and back-end applications using .NET, and have a deep understanding of the framework and its capabilities.",
     mdiIcon: "mdi-language-csharp",
   },
   {
     title: "Python",
     description:
-      "During my junior year of college, I took a machine learning class and ever since then have really been pulled into Python, loving both its simplicity and community. Learning Python was one of the quickest experiences, and I just kept finding myself saying 'that is so slick'. The shortcuts and tricks that are built into the language make it such a fun language to work with!",
+      "I've used python in a variety of projects, from small scripting tasks to training machine learning models to building large scale web applications. Professionally, I have built and maintained 30+ Django applications, and love the simplicity and power of the language.",
     mdiIcon: "mdi-language-python",
   },
   {
-    title: "Visual Studio (Code)",
+    title: "VS Code",
     description:
       "The majority of my coding life, I have written in Visual Studio. However, as of recently, I have really started to turn towards the much sleeker and faster Visual Studio Code. Now, whenver starting a project, I tend to start with VS Code and I don't really have to look back! The community support and extension library really does give you the power to shape the IDE into the exact tool you need for virtually any job.",
     mdiIcon: "mdi-microsoft-visual-studio-code",
@@ -59,34 +67,40 @@ const hardSkillList: Skill[] = [
 
 const softSkillList: Skill[] = [
   {
-    title: "SAFe",
+    title: "Agile",
     description:
-      "I have been working in a Scaled Agile Framework environment for 2 years now, along with a semester of classes structured to train Agile practices. At first, the rituals seemed to just take up time that could be better spent writing code, but as I mature in my field and am exposed to larger projects, I truly understand the value of SAFe and the organization and efficiency benefits that an enterprise gains through following a SAFe-like system.",
+      "I have worked in various Agile environments, ranging from an organization-wide Agile program including 300+ developers to a small team of 3. I've used various Agile methodologies, including Scrum, Kanban, and SAFe, and see the value in all versions of Agile.",
     mdiIcon: "mdi-image-filter-hdr",
   },
   {
     title: "Communication Abilities",
     description:
-      "My family and I have lived in 3 different countries, all during my time in highschool, which has thrown me into new social circles repeatedly and forced me to adapt to new situations quickly. One of the skills that resulted from all that upheaval was some great communication abilities. I am now comfortable in a room full of strangers, and don't mind one bit being the first one to open the floor and loosen the mood!",
+      "I believe that communication is the key to success in any relatiionship, personal or professional. I enjoy working together with other intelligent and motivated individuals to achieve a common goal, and am always looking for ways to learn and grow.",
     mdiIcon: "mdi-forum",
   },
   {
     title: "Constant Learning",
     description:
-      "I took my first coding class my freshman year of highschool and have not stopped learning since then! I love the software development industry because there is always new technologies rising up that are more efficient or more effective than the last. I believe learning is what keeps us sharp, and that constant flow of new tech is the perfect environment to be in a perpetual state of learning!",
+      "I approach life with curiosity and a desire to learn. This has helped me to grow and develop in many ways, especially in my career as a software developer; there is always new things to learn and ways to improve!",
     mdiIcon: "mdi-school",
   },
   {
     title: "Problem Solving",
     description:
-      "One of the best feelings is sitting down and chewing away at a hard problem, and after a time of hard work and energy finally reaching that moment where the solution just clicks into place. I am a junky for this sensation, which drives me to hunt down problems that need solutions. What better place to find this high than software development? I can confidently say I am addicted to solving problems, and my personal favorite way to satiate the addiction is to write code to improve others' lives.",
+      "There is nothing more satisfying than solving a problem, whether that be a difficult bug that has been confusing me for hours or a complex requirement from a client that my team needs to implement. I love the challenge of finding the best solution to a problem, and balancing the tradeoffs between different solutions.",
     mdiIcon: "mdi-lightbulb-on",
   },
   {
     title: "Positive Attitude",
     description:
-      "The one thing that can always make any situation better is doing your best to keep the positive angle. I do my best to apply this concept to everything in my life, and it has really paid off. Even when the work seems too much, or the weather seems to dreary, or the deadlines seem to creep up too quickly, a positive attitude has always set my foot in the right direction and kept me productive.",
+      "No matter what the situation, a positive attitude can only help. I try to approach every situation with a positive attitude, and it has helped me to be more productive and successful in my career.",
     mdiIcon: "mdi-comment-plus",
+  },
+  {
+    title: "Leadership",
+    description:
+      "I have led a couple of development teams, and have built a strong understanding of the importance of communication and collaboration in a team. I enjoy the challenge of building a plan and executing it across the team, and I love watching the team come together to achieve a common goal.",
+    mdiIcon: "mdi-account-group",
   },
 ];
 
@@ -164,12 +178,12 @@ const hobbies: Hobby[] = [
           <v-card :class="[mobile ? 'pa-5' : 'pa-10', 'w-100']">
             <h2 class="text-md-h2 text-h4 text-center mb-5">About Me</h2>
             <p class="text-md-h5 text-body-1">
-              My name is Joel Young and I love writing all things code! I enjoy
-              both the creativity and problem solving skills that are required
-              to write a good program. I thrive on applying what I know combined
-              with what I learned 30 minutes earlier from YouTube or
-              <a href="https://claude.ai" target="_blank">Claude</a> to solve
-              any problem thrown my way!
+              My name is Joel Young and I love writing code! I enjoy both the
+              creativity and problem solving skills involved in coding. I love
+              watching my ideas become reality or the feeling of implementing a
+              new technology to solve a problem more efficiently. Most
+              importantly, I love the feeling of creating something that can
+              help improve someone's life!
             </p>
             <v-row
               align="center"
@@ -226,10 +240,21 @@ const hobbies: Hobby[] = [
                 :text="skill.description"
               >
                 <template #title>
-                  <v-icon :icon="skill.mdiIcon" class="mr-3" /><span
-                    class="text-h6"
-                    >{{ skill.title }}</span
-                  >
+                  <v-icon
+                    v-if="skill.mdiIcon"
+                    :icon="skill.mdiIcon"
+                    class="mr-3"
+                  />
+                  <div>
+                    <v-img
+                      v-if="skill.svgIcon"
+                      :src="skill.svgIcon"
+                      width="24"
+                      height="24"
+                      class="mr-3"
+                    />
+                  </div>
+                  <span class="text-h6">{{ skill.title }} </span>
                 </template>
                 <template #text>
                   <span class="text-body-1">{{ skill.description }}</span>
@@ -272,32 +297,39 @@ const hobbies: Hobby[] = [
           Hobbies
         </h2>
         <v-row justify="center">
-          <v-col
-            v-for="hobby in hobbies"
-            :key="hobby.title"
-            md="6"
-            cols="12"
-            class="j-rounder"
-          >
-            <v-row>
-              <v-col md="5" cols="12">
-                <v-img
-                  :src="hobby.imgSrc"
-                  :alt="hobby.title"
-                  class="j-rounder j-raised"
-                  cover
-                  aspect-ratio="1"
-                />
-              </v-col>
-              <v-col md="7" cols="12">
-                <h3 class="text-md-h3 text-h5">
-                  {{ hobby.title }}
-                </h3>
-                <p class="text-md-h6 text-body-1">
-                  {{ hobby.description }}
-                </p>
-              </v-col>
-            </v-row>
+          <v-col md="6" cols="12">
+            <h3 class="text-md-h3 text-h5">Experiencing Nature</h3>
+            <p class="text-md-h6 text-body-1">
+              There is nothing more relaxing or freeing that escaping the
+              schedules and stress of everyday life to get out and experience
+              the beauty of nature. I love going on hikes, camping, fishing,
+              backpacking, and pretty much anything else that gets me outside!
+            </p>
+          </v-col>
+          <v-col md="6" cols="12">
+            <h3 class="text-md-h3 text-h5">Sports</h3>
+            <p class="text-md-h6 text-body-1">
+              I am not a pro at any sport, but I do enjoy playing a variety of
+              casual sports with friends and family. You can catch me playing
+              pickleball with friends, on the wall at my nearby climbing gym, or
+              playing pickup spikeball with my family!
+            </p>
+          </v-col>
+          <v-col md="6" cols="12">
+            <h3 class="text-md-h3 text-h5">Cooking</h3>
+            <p class="text-md-h6 text-body-1">
+              I love cooking and more importantly, eating great food! One of the
+              best experiences in life is sharing a meal with the ones you love,
+              laughing and enjoying the company and food together.
+            </p>
+          </v-col>
+          <v-col md="6" cols="12">
+            <h3 class="text-md-h3 text-h5">Gaming</h3>
+            <p class="text-md-h6 text-body-1">
+              Games are a great way to relax and have fun with anyone! I can be
+              found playing a variety of games, ranging from board games like
+              Catan or Wingspan to video games like Forza or Minecraft!
+            </p>
           </v-col>
         </v-row>
       </v-card>
