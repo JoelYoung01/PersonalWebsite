b74b80a4c7055dd9cf60f5bddb99606ab9cc7105
Joel Young [Jooser PC] <joeleyoung01@gmail.com>
2025-09-19 19:30:30 -0500

    Update vite version

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,6 @@ import { fileURLToPath, URL } from "node:url";
 
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
-import vueDevTools from "vite-plugin-vue-devtools";
 import vuetify, { transformAssetUrls } from "vite-plugin-vuetify";
 import AutoImport from "unplugin-auto-import/vite";
 
@@ -12,7 +11,6 @@ export default defineConfig({
     vue({
       template: { transformAssetUrls },
     }),
-    vueDevTools(),
     vuetify(),
     AutoImport({
       // https://github.com/unplugin/unplugin-auto-import?tab=readme-ov-file#configuration
