d1206dfe27249002a66675b30bcc2b6b721ea0b5
Joel Young [Jooser PC] <joeleyoung01@gmail.com>
2025-09-19 19:36:47 -0500

    Some cleanup from old stuff

--- src/router/projects.ts
+++ src/router/projects.ts
@@ -1,13 +1,5 @@
 const projectRoutes = [
   {
-    path: "map-draw-widget",
-    name: "Map Drawing Widget POC",
-    // route level code-splitting
-    // this generates a separate chunk (About.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import("../views/Projects/MapDrawWidget.vue"),
-  },
-  {
     path: "cost-estimate-tool",
     name: "Cost Estimate Tool",
     component: () => import("../views/Projects/CostEstimateTool.vue"),
